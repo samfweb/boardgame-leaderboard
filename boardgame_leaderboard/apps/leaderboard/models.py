cda0daf1569746b6f092c54e04be3e7ea910e8f9
Sam Webster <sam.f.webster1@gmail.com>
2020-03-24 23:45:54 +1100

    started on db

--- boardgame_leaderboard/apps/leaderboard/models.py
+++ boardgame_leaderboard/apps/leaderboard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db import connection
 
-# Create your models here.
+def get(table, *args):
+    col_names = []
+    for arg in args:
+        col_names += arg
+    if col_names:
+        sql_select = ' '.join(col_names)
+    else:
+        sql_select = '*'
+    
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT %s FROM %s;', [sql_select, table])
+        rows = dictfetchall(cursor)
+        return rows
+    
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]       
--- boardgame_leaderboard/apps/leaderboard/views.py
+++ boardgame_leaderboard/apps/leaderboard/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.leaderboard import models
 
 # Test games while I work our CSS
-games = [
-    {
-        'name': '7 Wonders Duel',
-        'players': '2',
-        'desc': 'We love it! Duel for the strongest military, science or culture.',
-        'last_updated': 'March 15, 2020',
-    }, 
-    {
-        'name': 'Splendour',
-        'players': '2-4',
-        'desc': 'Become the gem master',
-        'last_updated': 'March 15, 2020',
-    }
-]
+# games = [
+#     {
+#         'name': '7 Wonders Duel',
+#         'players': '2',
+#         'desc': 'We love it! Duel for the strongest military, science or culture.',
+#         'last_updated': 'March 15, 2020',
+#     }, 
+#     {
+#         'name': 'Splendour',
+#         'players': '2-4',
+#         'desc': 'Become the gem master',
+#         'last_updated': 'March 15, 2020',
+#     }
+# ]
+
+
 
 
 def home(request):
+    games = models.get("boardgame")
     context = {
         'games': games
     }
+    print()
     return render(request, 'leaderboard/home.html', context)
 
 
